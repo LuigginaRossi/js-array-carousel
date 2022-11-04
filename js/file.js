337ec6576b8c77c1c520c0baaed34d8a69edc4a8
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-04 19:37:02 +0100

    prova

--- js/file.js
+++ js/file.js
@@ -1,15 +1,50 @@
-const imgContainerEl = document.querySelector( ".images-container" );
-console.log( imgContainerEl)
+//creo array immagini
+const images = [
+    "imgs/01.webp",
+    "imgs/02.webp",
+    "imgs/03.webp",
+    "imgs/04.webp",
+    "imgs/05.webp"
+];
+console.log(images);
+//indice attuale è 0
+let currentImgIndex = 0;
+
+//prendo slide-containet da html
+const sliderContainerEl = document.querySelector( ".slider-container" );
+console.log( sliderContainerEl)
+//prendo i button da html
+const btnNext = document.querySelector( ".next" );
+const btnPrev = document.querySelector( ".prev" );
+
+console.log( btnNext, btnPrev);
+
+//aggiungo dentro al slider-container un tag img con indice 0
+sliderContainerEl.innerHTML += `<img src="${images[0]}" class="slider-img">`;
+    
+//prendo il tag img figlio di slider-container
+const sliderImgEl = document.querySelector( `.slider-container :nth-child(${ currentImgIndex + 1 })` );
+
+console.log(sliderImgEl)
 
 
-images=[
-    "01.webp",
-    "02.webp",
-    "03.webp",
-    "04.webp",
-    "05.webp"
-]
+btnNext.addEventListener( "click", function (){
+    console.log("hai cliccato il btn next");
 
-for (let i = 0; i < images.lenght; i++ ) {
-    imgContainerEl.innerHTML += `<img src="${images}" class="img-container">`;
-}
+    //incremento il contatore dell' valore images
+    currentImgIndex++;
+
+    sliderImgEl.src = images[currentImgIndex];
+    console.log(sliderImgEl)
+
+    
+});
+// for (let i = 0; i < images.lenght; i++ ) {
+//     //prendo il tag img figlio di slider-container
+//     const sliderImgEl = document.querySelector( ".slider-container :nth-child(${ currenImgIndex + 1})" );
+
+//     if (i === 0) {
+//         active = "active"
+//         sliderImgEl.classList.add("active")
+//     }
+// }
